8501e2d98a1c0ee1a478b42706a7744b4ad85f4b
Pinball Power <info@pinball-power.com>
2022-03-08 13:37:26 +0000

    Started working on SPI communication between Raspberry Pi and Pi Pico

---
+++ pi/readspi.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import time
+import spidev
+import RPi.GPIO as GPIO
+from struct import unpack
+
+notify_gpio=7
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(notify_gpio, GPIO.IN)
+
+spi = spidev.SpiDev()
+spi.open(1,0)
+spi.max_speed_hz=1000000
+
+print("SPI opened")
+
+
+done=False
+while not(done):
+    GPIO.wait_for_edge(notify_gpio, GPIO.RISING)
+    print("received notify")
+
+    header=bytes(spi.readbytes(4))
+    print(header);
+
+    (length,packettype) = unpack(">HH",header);
+
+    if packettype==0:
+        continue
+
+    if length<=4:
+        continue
+
+    print(packettype, length)
+
+    buf=spi.readbytes(length-4)
+
+    
+GPIO.cleanup()
